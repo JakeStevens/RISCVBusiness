f38350b2d94331cfb2abcf3c940ed77f2455e2a8
JohnSkubic <jskubic@purdue.edu>
2016-06-01 00:24:50 -0400

    Added licensing information

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Design documents and project information for the RISC-V Business project can be found here:
 
 https://wiki.itap.purdue.edu/display/RISC/RISC-V-Business+Home
+
+Refer to the LICENSE file for licensing information.
--- tb/tb_RISCVBusiness.sv
+++ tb/tb_RISCVBusiness.sv
@@ -1,17 +1,27 @@
-/***********************************************************
- *
- *  Author: John Skubic
- *
- *  tb_RISCVBusines
- *
- *  Testbench will allow the core to run while connected 
- *  to a ram module.  The ram module should be loaded with
- *  the program being run.  When the core signals a halt 
- *  condition, the ram is dumped to a hex file in the 
- *  Intel format.  
- *
- * *********************************************************
- */
+/*
+*		Copyright 2016 Purdue University
+*		
+*		Licensed under the Apache License, Version 2.0 (the "License");
+*		you may not use this file except in compliance with the License.
+*		You may obtain a copy of the License at
+*		
+*		    http://www.apache.org/licenses/LICENSE-2.0
+*		
+*		Unless required by applicable law or agreed to in writing, software
+*		distributed under the License is distributed on an "AS IS" BASIS,
+*		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*		See the License for the specific language governing permissions and
+*		limitations under the License.
+*
+*
+*		Filename:		  tb_RISCVBusiness.sv	
+*
+*		Created by:		John Skubic
+*		Email:				jskubic@purdue.edu
+*		Date Created:	06/01/2016
+*		Description:	Testbench for running RISCVBusiness until a halt condition.
+*		              A hexdump of memory will occur after the halt condition.
+*/
 
 
 `include "ram_if.vh"
