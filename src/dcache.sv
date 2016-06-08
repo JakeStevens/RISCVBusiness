943b000cd1d5c66527006c38f6653a417f9aebf1
JohnSkubic <jskubic@purdue.edu>
2016-06-07 21:30:20 -0400

    fixed the signal directions in the icache and dcache

--- src/dcache.sv
+++ src/dcache.sv
@@ -1,25 +1,25 @@
 /*
-*		Copyright 2016 Purdue University
-*		
-*		Licensed under the Apache License, Version 2.0 (the "License");
-*		you may not use this file except in compliance with the License.
-*		You may obtain a copy of the License at
-*		
-*		    http://www.apache.org/licenses/LICENSE-2.0
-*		
-*		Unless required by applicable law or agreed to in writing, software
-*		distributed under the License is distributed on an "AS IS" BASIS,
-*		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*		See the License for the specific language governing permissions and
-*		limitations under the License.
-*
-*
-*		Filename:     dcache.sv
-*
-*		Created by:   John Skubic
-*		Email:        jskubic@purdue.edu
-*		Date Created: 06/01/2016
-*		Description:  Data Cache	
+*   Copyright 2016 Purdue University
+*   
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*   
+*       http://www.apache.org/licenses/LICENSE-2.0
+*   
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*   
+*   
+*   Filename:     dcache.sv
+*   
+*   Created by:   John Skubic
+*   Email:        jskubic@purdue.edu
+*   Date Created: 06/01/2016
+*   Description:  Data Cache	
 */
 
 `include "ram_if.vh"
@@ -31,12 +31,12 @@ module dcache (
 );
 
   //passthrough layer
-  assign ram_out_if.addr  = ram_in_if.addr;
-  assign ram_out_if.ren   = ram_in_if.ren;
-  assign ram_out_if.wen   = ram_in_if.wen;
-  assign ram_out_if.wdata = ram_in_if.wdata;
+  assign ram_in_if.addr  = ram_out_if.addr;
+  assign ram_in_if.ren   = ram_out_if.ren;
+  assign ram_in_if.wen   = ram_out_if.wen;
+  assign ram_in_if.wdata = ram_out_if.wdata;
   
-  assign ram_in_if.rdata  = ram_out_if.rdata;
-  assign ram_in_if.busy   = ram_out_if.busy;
+  assign ram_out_if.rdata  = ram_in_if.rdata;
+  assign ram_out_if.busy   = ram_in_if.busy;
 
 endmodule
