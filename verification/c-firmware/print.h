53cb2b2f8d606ba4d821a338023bb2e25eee6fb2
Jacob Stevens <steven69@purdue.edu>
2016-09-27 21:05:14 -0400

    Add printing to C firmware

---
+++ verification/c-firmware/print.h
@@ -0,0 +1,29 @@
+/*
+*   Copyright 2016 Purdue University
+*   
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*   
+*       http://www.apache.org/licenses/LICENSE-2.0
+*   
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*
+*
+*   Filename:     print.h
+*
+*   Created by:   Jacob R. Stevens
+*   Email:        steven69@purdue.edu
+*   Date Created: 09/27/2016
+*   Description: Header defining print functions 
+*/
+
+#ifndef PRINT_H
+#define PRINT_H
+void print_str(char *str);
+void print_char(char c);
+#endif
