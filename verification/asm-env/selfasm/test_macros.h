647c26466ffecdddf1497fbb87659235c5ce0f05
Wengyan Chan <cwengyan@purdue.edu>
2019-09-17 00:46:50 -0400

    Changed STORE_SASA_FROM_MEM to allow word addr offset
    Added additional parameter "reg_offset" that takes a register with
    word offset value. This allows multiple entries to be loaded to the
    SASA table from a lable.
    
    The functionality is tested with the sparce_single_skip.S
    The macro is in a working state.

--- verification/asm-env/selfasm/test_macros.h
+++ verification/asm-env/selfasm/test_macros.h
@@ -22,24 +22,17 @@ test_ ## testnum: \
 #define CALC_SASA_VAL(reg1, reg2, condition, insts_to_skip) \
 (((reg1) << 11) + ((reg2) << 6) + ((condition << 5)) + ((insts_to_skip)));
 
-#define STORE_SASA_VAL(skip_pc, reg1, reg2, condition, insts_to_skip, tempreg1, tempreg2) \
-la tempreg1, skip_pc; \
-slli tempreg1, tempreg1, 16; \
-lui  tempreg2, CALC_SASA_VAL(reg1, reg2, condition, insts_to_skip); \
-srli tempreg2, tempreg2, 12; \
-or   tempreg1, tempreg1, tempreg2;\
-sw   tempreg1,;
-
-#define STORE_SASA_FROM_MEM(mem_label, tempreg1, tempreg2) \
-la tempreg2, mem_label; \
-lw tempreg1, 0(tempreg2); \
+#define STORE_SASA_FROM_MEM(mem_label, reg_offset, tempreg1, tempreg2) \
+slli tempreg1, reg_offset, 3; \
+la   tempreg2, mem_label; \
+add  tempreg2, tempreg2, tempreg1; \
+lw   tempreg1, 0(tempreg2); \
 slli tempreg1, tempreg1, 14; \
-lw tempreg2, 4(tempreg2); \
-or tempreg1, tempreg1, tempreg2; \
-lui tempreg2, SPARCE_ADDR; \
+lw   tempreg2, 4(tempreg2); \
+or   tempreg1, tempreg1, tempreg2; \
+lui  tempreg2, SPARCE_ADDR; \
 srli tempreg2, tempreg2, 12; \
-sw tempreg1, 0(tempreg2);
-
+sw   tempreg1, 0(tempreg2);
 
 #define CREATE_SASA_MEMORY_ENTRIES(skip_label, reg1, reg2, condition, insts_to_skip) \
 .word skip_label; \
