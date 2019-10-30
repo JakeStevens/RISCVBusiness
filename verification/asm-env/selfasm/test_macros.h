66d748c6f677c79d5950a20483249f9b9739256b
Niraj Menon <nirajmmenon@gmail.com>
2019-10-30 17:53:35 -0400

    fixed sparce macro to accommodate larger SASA_ADDR

--- verification/asm-env/selfasm/test_macros.h
+++ verification/asm-env/selfasm/test_macros.h
@@ -31,8 +31,7 @@ lw   tempreg1, 0(tempreg2); \
 slli tempreg1, tempreg1, 14; \
 lw   tempreg2, 4(tempreg2); \
 or   tempreg1, tempreg1, tempreg2; \
-lui  tempreg2, SPARCE_ADDR; \
-srli tempreg2, tempreg2, 12; \
+li   tempreg2, SPARCE_ADDR; \
 sw   tempreg1, 0(tempreg2);
 
 #define CREATE_SASA_MEMORY_ENTRIES(skip_label, reg1, reg2, condition, insts_to_skip) \
