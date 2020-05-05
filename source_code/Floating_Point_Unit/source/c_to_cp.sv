5cb38b1759b75aab7697f5a078516fa12e722e74
Xinlue Liu <42654960+XinlueLiu@users.noreply.github.com>
2020-05-05 01:41:23 -0400

    Update c_to_cp.sv

--- source_code/Floating_Point_Unit/source/c_to_cp.sv
+++ source_code/Floating_Point_Unit/source/c_to_cp.sv
@@ -1,12 +1,15 @@
 module c_to_cp
   (
-   input [26:0] frac2_input,
-   output reg [26:0] frac2_output
+   input [25:0] frac2_input,//25
+   input [26:0] frac2_signedin,
+   output reg [25:0] frac2_output,//25
+   output reg [26:0] frac2_signedout
    );
  
    always_comb begin
    //find the two's complement of the frac2
-      frac2_output[26:0] = (~frac2_input[26:0] + 1'b1);
+      frac2_output[25:0] = (~frac2_input[25:0] + 1'b1);//25
+      frac2_signedout[26:0] = (~frac2_signedin[26:0] + 1'b1);
    end
    
    
