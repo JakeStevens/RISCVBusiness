6a98822c8aa7d768562631d2973f0f5abd8c15dc
Xinlue Liu <42654960+XinlueLiu@users.noreply.github.com>
2020-06-04 00:45:50 -0400

    Update sub_26b.sv

--- source_code/Floating_Point_Unit/source/sub_26b.sv
+++ source_code/Floating_Point_Unit/source/sub_26b.sv
@@ -1,18 +1,24 @@
 //By            : Joe Nasti
-//Last Updated  : 7.16.18
+//Modified by   : Xinlue Liu
+//Last Updated  : 6/1/20
 //
 //Module Summary: 
 //    adds two signed 26 bit fraction values
 //
 //Inputs:
 //    frac1/2 - signed 26 bit values with decimal point fixed after second bit
+//    frac1_s/frac2_s - 2's complements of the two floating points
+//    exp_determine - signal indicator that indicates which subtraction operation it is going to perform
 //Outputs:
 //    sum     - output of sum operation regardless of overflow
 //    ovf     - high if an overflow has occured 
  
 module sub_26b(
 	input      [26:0] frac1,
-	input      [26:0] frac2, 
+	input      [26:0] frac2,
+	input      [26:0] frac1_s,
+	input      [26:0] frac2_s,
+	input reg exp_determine,
 	output reg [26:0] sum, 
 	output reg        ovf
 );
@@ -21,18 +27,12 @@ module sub_26b(
    reg [26:0] 		  temp_sum;
    
 always_comb begin
-   //find the two's complement of the frac2
-   /*if (frac2[26] == 1) begin
-      frac2_compute[26] = frac2[26];
-      frac2_compute[25:0] = (~frac2[25:0] + 1'b1);
-   end else if (frac2[26] == 0) begin
-      frac2_compute = frac2;
-   end
-   sum = frac1 + frac2_compute;*/
-   //sum = ~temp_sum;
-   temp_sum = frac1 + frac2;
-   //sum = frac1 + frac2;
+   if (exp_determine == 1) begin
+   sum = frac1 + frac2;
+   end else begin
+   temp_sum = frac1_s + frac2_s;
    sum[26:0] = ~temp_sum[26:0] + 1'b1;
+   end
    
    ovf = 0;
    
@@ -45,6 +45,17 @@ always_comb begin
       ovf = 1;
       sum[26] = 0;
    end
+
+   if(frac1_s[26] == 1 && frac2_s[26]== 1 && sum[26] == 0) begin
+      ovf = 1;
+      sum[26] = 1;
+   end
+   
+   if(frac1_s[26] == 0 && frac2_s[26]== 0 && sum[26] == 1) begin
+      ovf = 1;
+      sum[26] = 0;
+   end
   
 end
 endmodule
+
