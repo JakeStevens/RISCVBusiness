0256891fda650057e3c15a446b38bcae2e71b256
Xinlue Liu <42654960+XinlueLiu@users.noreply.github.com>
2020-06-04 00:53:13 -0400

    Update SUB_step2.sv

--- source_code/Floating_Point_Unit/source/SUB_step2.sv
+++ source_code/Floating_Point_Unit/source/SUB_step2.sv
@@ -25,11 +25,6 @@ module SUB_step2
       else exp_max_out = exp_max_in;
    end
 
-   /*always_comb begin : exp_determine
-   if (bothnegsub) begin
-	exp_determine = 1'b1;
-   end
-   end*/
    //change to signed value. either {0, frac} or {0, ~frac} if sign is 1
    u_to_s change_to_signed1(
 			    .sign(sign1),
@@ -47,23 +42,10 @@ module SUB_step2
    c_to_cp change_to_complement(
 				.frac2_input(frac2),
 				.frac2_signedin(frac2_signed),
-<<<<<<< HEAD
-				.frac2_output(frac2_complement), //unsigned output
-				.frac2_signedout(frac2_signedout) //signed output
-				);
-   //perform subtraction
-=======
 				.frac2_output(frac2_complement),
 				.frac2_signedout(frac2_signedout)
 				);
-	/*c_to_cp change_to_complement(
-				.frac2_input(frac2_signed),
-				//.frac2_signedin(frac2_signed),
-				.frac2_output(frac2_complement)
-				//.frac2_signedout(frac2_signedout)
-				);*/
-   
->>>>>>> fa4bb25b0b7f0da1f3fd01824f72305558abd74b
+   //perform subtraction
    sub_26b sub_signed_fracs(
 			     .exp_determine(exp_determine),
 			     .frac1({1'b0,frac1[25:0]}),
@@ -73,14 +55,6 @@ module SUB_step2
 			     .sum(sum_signed),
 			     .ovf(carry_out)
 			     );
-/*sub_26b sub_signed_fracs(
-			     .frac1({1'b0,frac1_signed[26:1]}),
-			     .frac2(frac2_complement),
-			     .sum(sum_signed),
-			     .ovf(carry_out)
-			     );*/
-  //assign sum = {sum_signed[24:0],1'b0};
-  //assign sign_out = sum_signed[26];
 
    s_to_u_new change_to_unsigned_new(
 			     .frac_signed(sum_signed),
