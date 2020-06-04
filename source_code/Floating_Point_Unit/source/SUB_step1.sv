bb1a6b241a2dedf8989cfdecc9a2e13ee4b67785
Xinlue Liu <42654960+XinlueLiu@users.noreply.github.com>
2020-06-04 00:45:07 -0400

    Update SUB_step1.sv

--- source_code/Floating_Point_Unit/source/SUB_step1.sv
+++ source_code/Floating_Point_Unit/source/SUB_step1.sv
@@ -1,8 +1,9 @@
 //By            : Joe Nasti
-//Last updated  : 7/23/18
+//Edited by	: Xinlue Liu
+//Last updated  : 6/1/20
 //
 //Module summary:
-//    First step for addition operation in three-step pipline.
+//    First step for subtraction operation in three-step pipline.
 //    Shifts smaller fraction by difference in exponents
 //
 //Inputs:
@@ -13,8 +14,10 @@
 //    sign_not_shifted     - sign of the floating point that does not get shifted
 //    frac_not_shifted     - fraction of the floating point that does not get shifted
 //    exp_max              - max exponent of the two given floating points
+//    cmp		   - indicator of which floating point is bigger or smaller
 module SUB_step1
   (
+   input 	bothnegsub,
    input [31:0]  floating_point1_in,
    input [31:0]  floating_point2_in,
    output 	 sign_shifted,
@@ -24,7 +27,8 @@ module SUB_step1
    output [7:0]  exp_max,
    output 	 cmp
    );
-
+   reg [31:0] floating_point1_in_temp;
+   reg [31:0] floating_point2_in_temp;
    reg  [7:0] 	 unsigned_exp_diff;
    reg 		 cmp_out; //exp1 >= exp2 -> cmp_out == 0
                           //exp1 <  exp2 -> cmp_out == 1
@@ -32,20 +36,29 @@ module SUB_step1
    wire [31:0] 	 floating_point_not_shift;
    reg  [31:0] 	 shifted_floating_point;
    reg [22:0] 	 temp_frac_shifted;
-   
-   //compare the expoents of two floating points
+
+   always_comb begin: determine_input
+	if (bothnegsub == 1'b1) begin
+		floating_point1_in_temp = floating_point2_in;
+		floating_point2_in_temp = floating_point1_in;
+	end else begin
+		floating_point1_in_temp = floating_point1_in;
+		floating_point2_in_temp = floating_point2_in;
+   	end
+   end
+   //compare the exponents of two floating points
    int_compare cmp_exponents (
-			      .exp1(floating_point1_in[30:23]), 
-			      .exp2(floating_point2_in[30:23]),
+			      .exp1(floating_point1_in_temp[30:23]), 
+			      .exp2(floating_point2_in_temp[30:23]),
 			      .u_diff(unsigned_exp_diff),
 			      .cmp_out(cmp_out)
 			      );
    //determine which one to shift
    //shift the smaller exponent
-   assign floating_point_shift = cmp_out ? floating_point1_in : floating_point2_in;
-   assign floating_point_not_shift = cmp_out ? floating_point2_in : floating_point1_in;
+   assign floating_point_shift = cmp_out ? floating_point1_in_temp : floating_point2_in_temp;
+   assign floating_point_not_shift = cmp_out ? floating_point2_in_temp : floating_point1_in_temp;
    //set the result exponent to the bigger exponent between X and Y
-   assign exp_max = cmp_out ? floating_point2_in[30:23] : floating_point1_in[30:23];
+   assign exp_max = cmp_out ? floating_point2_in_temp[30:23] : floating_point1_in_temp[30:23];
 
    //right shift the smaller fp the amount of the difference of two fps.
    //right_shift_minus shift_frac (
